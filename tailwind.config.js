148e3de5910b0471c6ff03fa6562027041c5c1e3
Imam Hatris Ekaputra <ggmukmin@gmail.com>
2023-08-13 23:18:11 +0700

    no message

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,19 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container: {
+      center: true,
+      padding: '15px',
+    },
+
     extend: {
       colors: {
         primary: '#38bdf8',
         dark: '#1e293b',
+      },
+
+      screens: {
+        '2xl': '1320px',
       },
     },
   },
